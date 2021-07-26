d570b0e0827da651e0649b9c6879c7d55a069e5b
Yoshizawa <Sumire-Yoshizawa@github.com>
2021-07-26 14:08:59 +0700

    nextjs 1

---
+++ components/Navbar/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+export default function Navbar() {
+    return (
+        <div>
+            <h1>
+                <a>Navbar</a>
+                <Link href="/about"><a>About</a></Link>
+            </h1>
+  </div>
+    
+    )
+}
+
+
